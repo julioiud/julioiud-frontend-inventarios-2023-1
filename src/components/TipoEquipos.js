62c71cc73524cb0940aa28440947274a02fafab8
Julio Martinez <julio.martinez@iudigital.edu.co>
2023-04-01 03:43:37 +0200

    :sparkles: :lipstick: :construction: guardado tipo equipo & modal ui

--- src/components/TipoEquipos.js
+++ src/components/TipoEquipos.js
@@ -1,12 +1,18 @@
 import dayjs from 'dayjs'
 import React, { useEffect, useState } from 'react'
-import { getTipoEquipos } from '../services/TipoEquipoService'
-export default function TipoEquipos({title}) {
+import { createTipoEquipo, getTipoEquipos } from '../services/TipoEquipoService'
+import Modal from './ui/Modal'
 
+export default function TipoEquipos() {
+const title= 'Tipo de Equipo'
 const [tipoEquipos, setTipoEquipos] = useState([])
 const [query, setQuery] = useState(true)
 const [loading, setLoading] = useState(true)
 const [error, setError]= useState(false)
+const [tipoEquipo, setTipoEquipo] = useState({
+  nombre: ''
+})
+const [loadingSave, setLoadingSave] = useState(false)
 
 const listTipoEquipos = async () => {
   try{
@@ -35,39 +41,45 @@ const changeSwitch = () => {
   setQuery(!query)
 }
 
+const handleChange = (e) => {
+  setTipoEquipo({
+    ...tipoEquipo,
+    [e.target.name]: e.target.value
+  })
+}
+
+const saveTipoEquipo = async () => {
+  try{
+    setError(false)
+    setLoadingSave(true)
+    const response = await createTipoEquipo(tipoEquipo)
+    console.log(response)
+    setTipoEquipo({nombre: ''})
+    listTipoEquipos()
+    setTimeout(() => {
+      setLoadingSave(false)
+    }, 500)
+  }catch(e){
+    console.log(e)
+    setError(true)
+    setLoadingSave(false)
+  }
+}
+
+const closeModal = () => {
+  setTipoEquipo({nombre: ''})
+}
 
   return (
     <>
-        <div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-          <div className="modal-dialog">
-            <div className="modal-content">
-              <div className="modal-header">
-                <h1 className="modal-title fs-5" id="exampleModalLabel">Nuevo xxxxxxx</h1>
-                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-              </div>
-              <div className="modal-body">
-                <form>
-                  <div className="mb-3">
-                    <label htmlFor="recipient-name" 
-                    className="col-form-label">
-                      Nombre:
-                    </label>
-                    <input 
-                      type="text" 
-                      className="form-control" 
-                      id="recipient-name"
-                    />
-                  </div>
-                </form>
-              </div>
-              <div className="modal-footer">
-                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-                <button type="button" className="btn btn-primary">Enviar</button>
-              </div>
-            </div>
-          </div>
-        </div>
-
+        <Modal 
+          title={title}
+          closeModal={closeModal}
+          handleChange={handleChange}
+          tipoEquipo={tipoEquipo}
+          loadingSave={loadingSave}
+          saveTipoEquipo={saveTipoEquipo}
+        />
         <div className="form-check form-switch">
           <input 
             className="form-check-input" 
@@ -96,7 +108,7 @@ const changeSwitch = () => {
         {
           error && 
           (
-            <div class="alert alert-danger" role="alert">
+            <div className="alert alert-danger" role="alert">
               Ha ocurrido un error
             </div>
           )
@@ -127,13 +139,13 @@ const changeSwitch = () => {
                 {
                   tipoEquipos.map((tipoEquipo, index) => {
                     return (
-                      <tr>
+                      <tr key={tipoEquipo._id}>
                         <th scope="row">{index + 1}</th>
                         <td>{tipoEquipo.nombre}</td>
                         <td>{tipoEquipo.estado ? 'Activo' : 'Inactivo'}</td>
                         <td>{dayjs(tipoEquipo.fechaCreacion).format('YYYY-MM-DD')}</td>
                         <td>{dayjs(tipoEquipo.fechaActualizacion).format('YYYY-MM-DD')}</td>
-                        <td><button type="button" class="btn btn-success">Editar</button></td>
+                        <td><button type="button" className="btn btn-success">Editar</button></td>
                       </tr>
                     )
                   })
@@ -142,8 +154,6 @@ const changeSwitch = () => {
               </table>
             )
           }
-
-
         </div>
     </>
   )
